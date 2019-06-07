06af70bac1f1087c54faa4df819799dfc85a4e67
Petar <petarpnp@gmail.com>
2019-06-07 23:09:55 +0200

    Diagonal Star task done

---
+++ Java-Programming-Masterclass_Tim-Buchalka/DiagonalStar/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        DiagonalStar.printSquareStar(5);
+        System.out.println();
+        DiagonalStar.printSquareStar(8);
+        System.out.println();
+        DiagonalStar.printSquareStar(16);
+
+    }
+}
